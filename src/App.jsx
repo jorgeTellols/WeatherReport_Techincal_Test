c8fbf4343b7cc54fea0b580c50523d9ea1fd06c7
jorgeTellols <jorgetellolsdaw@gmail.com>
2024-08-01 16:45:26 +0200

    Project begins

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="weather-app">
+      <div className="language-buttons-container">
+        <button className="language-button" type="button">English</button>
+        <button className="language-button" type="button">Español</button>
+      </div>
+      <div className="content">
+      </div>
+    </div>
+  )
+}
+
+export default App
