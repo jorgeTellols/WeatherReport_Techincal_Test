811e854e935e7349189d5f264dcca8e5570eba24
jorgeTellols <jorgetellolsdaw@gmail.com>
2024-08-01 22:08:24 +0200

    Working on sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import './App.css';
+import Sidebar from './widgets/w-sidebar';
 
 function App() {
 
   return (
     <div className="weather-app">
+      <Sidebar></Sidebar>
       <div className="language-buttons-container">
         <button className="language-button" type="button">English</button>
         <button className="language-button" type="button">Español</button>
---
+++ src/widgets/w-sidebar.jsx
@@ -0,0 +1,14 @@
+import "./w-sidebar.css";
+
+function Sidebar() {
+
+    return (
+        <div className="sidebar">
+            <span>Dublin 🍀</span>
+            <span>Roma 🍕</span>
+            <span>Tokio 🗼</span>
+        </div>
+    )
+}
+
+export default Sidebar;
